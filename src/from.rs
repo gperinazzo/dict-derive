d7ad060fb4e75c3d3afb1c97c46ffcd98a02dc0b
Guilherme Perinazzo <gperinazzo@gmail.com>
2019-06-15 18:36:51 -0300

    Move from impl to it's own module

---
+++ src/from.rs
@@ -0,0 +1,108 @@
+use quote::{quote, quote_spanned};
+use syn::export::TokenStream2;
+use syn::spanned::Spanned;
+
+use syn::{Data, DeriveInput, Field, Ident};
+
+fn is_option(ty: &syn::Type) -> bool {
+    let path = match *ty {
+        syn::Type::Path(ref p) if p.qself.is_none() => &p.path,
+        _ => return false,
+    };
+
+    path.segments
+        .iter()
+        .map(|segment| segment.ident.to_string())
+        .any(|x| x.as_str() == "Option")
+}
+
+fn map_extraction(field: &Field) -> TokenStream2 {
+    let ident = match &field.ident {
+        Some(i) => i,
+        None => {
+            return syn::Error::new(field.span(), "Unnamed fields are not supported")
+                .to_compile_error()
+        }
+    };
+
+    let name = ident.to_string();
+
+    let function = if is_option(&field.ty) {
+        Ident::new("extract_optional", field.ty.span())
+    } else {
+        Ident::new("extract_required", field.ty.span())
+    };
+
+    quote_spanned! {ident.span()=>
+        #ident: #function(dict, #name)?
+    }
+}
+
+fn extraction_functions() -> syn::export::TokenStream2 {
+    quote! {
+        fn map_exception(name: &str, _: PyErr) -> PyErr {
+            PyErr::new::<TypeError, _>(format!("Unable to convert key: {}", name))
+        }
+
+        fn extract_required<'a, T>(dict: &'a PyDict, name: &str) -> PyResult<T>
+        where T: FromPyObject<'a>{
+            match dict.get_item(name) {
+                Some(v) => FromPyObject::extract(&v)
+                    .map_err(|err| map_exception(name, err)),
+                None => Err(PyErr::new::<ValueError, _>(format!("Missing required key: {}", name)))
+            }
+        }
+
+        fn extract_optional<'a, T>(dict: &'a PyDict, name: &str) -> PyResult<std::option::Option<T>>
+        where T: FromPyObject<'a>{
+            match dict.get_item(name) {
+                Some(v) => FromPyObject::extract(&v)
+                    .map_err(|err| map_exception(name, err)),
+                None => Ok(None),
+            }
+        }
+    }
+}
+
+pub fn from_impl(ast: DeriveInput) -> TokenStream2 {
+    let struct_data = match ast.data {
+        Data::Struct(s) => s,
+        Data::Enum(e) => {
+            return syn::Error::new(e.enum_token.span, "Deriving enums is not supported")
+                .to_compile_error();
+        }
+        Data::Union(u) => {
+            return syn::Error::new(u.union_token.span, "Deriving unions is not supported")
+                .to_compile_error();
+        }
+    };
+
+    let extractions = struct_data.fields.into_iter().map(map_extraction);
+
+    let name = ast.ident;
+    let (_, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let functions = extraction_functions();
+
+    quote! {
+        impl<'source> ::pyo3::FromPyObject<'source> for #name #ty_generics #where_clause {
+            fn extract(obj: &'source ::pyo3::types::PyAny) -> ::pyo3::PyResult<Self> {
+                use ::pyo3::{FromPyObject, PyErr, PyResult};
+                use ::pyo3::exceptions::{ValueError, TypeError};
+                use ::pyo3::types::PyDict;
+                let dict = <pyo3::types::PyDict as ::pyo3::PyTryFrom>::try_from(obj)
+                    .map_err(
+                        |_| PyErr::new::<TypeError, _>("Invalid type to convert, expected dict")
+                    )?;
+
+                #functions
+
+                ::std::result::Result::Ok(#name {
+                    #(#extractions),*
+                })
+
+            }
+        }
+
+    }
+}
