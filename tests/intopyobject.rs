6a9d7b734810a3ebff1e33ee0334f5586987d011
Guilherme Perinazzo <gperinazzo@gmail.com>
2019-06-15 22:29:42 -0300

    Add IntoPyObject tests

---
+++ tests/intopyobject.rs
@@ -0,0 +1,56 @@
+extern crate pyo3;
+use dict_derive::IntoPyObject;
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+
+#[derive(IntoPyObject, Debug)]
+struct User {
+    name: Option<String>,
+    email: String,
+    age: u16,
+}
+
+#[test]
+fn test_conversion() -> PyResult<()> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
+    {
+        let user = User {
+            name: None,
+            email: "tester@tests.com".to_owned(),
+            age: 27,
+        };
+        let obj = user.into_object(py);
+        let dict: &PyDict = obj.cast_as(py)?;
+
+        assert!(dict.get_item("name").is_some());
+        assert!(dict.get_item("email").is_some());
+        assert!(dict.get_item("age").is_some());
+
+        let name: Option<&str> = dict.get_item("name").unwrap().extract()?;
+        assert_eq!(name, None);
+
+        let email: &str = dict.get_item("email").unwrap().extract()?;
+        assert_eq!(email, "tester@tests.com");
+
+        let age: u16 = dict.get_item("age").unwrap().extract()?;
+        assert_eq!(age, 27);
+    }
+
+    {
+        let user = User {
+            name: Some("Test".to_owned()),
+            email: "tester@tests.com".to_owned(),
+            age: 27,
+        };
+        let obj = user.into_object(py);
+        let dict: &PyDict = obj.cast_as(py)?;
+
+        assert!(dict.get_item("name").is_some());
+
+        let name: Option<&str> = dict.get_item("name").unwrap().extract()?;
+        assert_eq!(name, Some("Test"));
+    }
+    Ok(())
+}
